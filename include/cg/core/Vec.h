a2a6964aa57cd1bc375a56184970c4d06d2336cb
SathvikNayak92 <78486961+sathvik7vk@users.noreply.github.com>
2026-04-06 02:59:47 +0000

    vector comp

--- include/cg/core/Vec.h
+++ include/cg/core/Vec.h
@@ -240,19 +240,27 @@ namespace cg::core
 					The Measure: This is what the Dot Product tracks.
 						Example: If you are pushing a box, your force is "aligned" with the box's movement if you’re pushing it straight ahead.
 
-							Orientation:
-								Orientation describes how an object is positioned or rotated in space relative to a fixed frame. It’s about tilt and facing.
-									The Vibe: Which way is "up" or "forward" for this specific object?
-										The Measure: This is often defined using the Cross Product to find a "normal" vector (a line sticking straight out of a surface).
-											Example: A smartphone knows its "orientation" (portrait vs. landscape) by sensing which way gravity is pulling relative to its screen.
-												*/
+						| Result       | Meaning                                     |
+						| ------------ | ------------------------------------------- |
+						| **Positive** | Vectors point roughly in the same direction |
+						| **Zero**     | Vectors are perpendicular (90°)             |
+						| **Negative** | Vectors point in opposite directions        |
 
-	/*
-		If
-			result > 0 => a, b, c are in counter-clockwise direction
-				result < 0 => a, b, c are in clockwise direction
-					result == 0 => a,b,c are collinear
-						*/
+
+		Orientation:
+			Orientation describes how an object is positioned or rotated in space relative to a fixed frame. It’s about tilt and facing.
+				The Vibe: Which way is "up" or "forward" for this specific object?
+					The Measure: This is often defined using the Cross Product to find a "normal" vector (a line sticking straight out of a surface).
+					Example: A smartphone knows its "orientation" (portrait vs. landscape) by sensing which way gravity is pulling relative to its screen.
+		
+
+						
+						If
+							result > 0 => a, b, c are in counter-clockwise direction
+							result < 0 => a, b, c are in clockwise direction
+							result == 0 => a,b,c are collinear
+	*/
+			
 	template <typename T>
 	constexpr static int orient(const Vec<T, 2> &a, const Vec<T, 2> &b, const Vec<T, 2> &c)
 	{
